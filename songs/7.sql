4da34ae567a70b3bf185b6de592dfec8cf92c293
Eric Jones <125319267+GhostEJ22@users.noreply.github.com>
2023-07-15 16:10:24 -0400

    Delete songs directory

--- songs/7.sql
+++
@@ -1,4 +0,0 @@
---SELECT AVG(energy) from songs WHERE artist_id IN
-  --(SELECT id from artists where name = "Drake");
---OR
-SELECT AVG(energy) FROM songs JOIN artists ON artist_id = artists.id WHERE artists.name = "Drake";
